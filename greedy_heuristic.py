d1a1ff778f869f5c5d3d73248f3bf197ff6ad3e4
sukeshsangam <ssang001@odu.edu>
2017-09-30 02:53:40 -0400

    Added sokoban game solver using bfs, dfs, greedy, A*

---
+++ greedy_heuristic.py
@@ -0,0 +1,201 @@
+import sys
+import numpy as np
+from collections import deque
+import timeit
+robot=[]
+walls=[]
+storage=[]
+box=[]
+directions={}
+directions['N']=[0,-1]
+directions['E']=[1,0]
+directions['W']=[-1,0]
+directions['S']=[0,1]
+start_time=0
+#function with own heuristic
+
+def heuristic(box_ls,storage_ls,point_ls):
+	distance=[]
+	p=point_ls[:]
+	for h in box_ls:
+		dis_h_p=sum(abs(np.subtract(h,p)))
+		dis_temp=[]
+		for s in storage_ls:
+			a=sum(abs(np.subtract(h,s)))+dis_h_p
+			dis_temp.append(a)
+		distance.append(min(dis_temp))
+	dis_value=sum(distance)
+	return dis_value
+#function for reading a file
+def print_char(filename):
+	f = open(filename, 'r')
+	i=0
+	j=0
+	while True:
+		char=f.read(1)
+		temp=[]
+		if char: 
+			temp.append(i)
+			temp.append(j)
+			if char == "O":
+				walls.append(temp)
+			if char == "R":
+				robot.append(temp)
+			if char == "S":
+				storage.append(temp)
+			if char == "B":
+				box.append(temp)
+			if char == ".":
+				box.append(temp)
+				storage.append(temp)
+			if char == "$":
+				robot.append(temp)
+				storage.append(temp)
+			if char == "\n":
+				i=0
+				j=j+1
+			else:
+				i=i+1
+		else:
+			break
+if len(sys.argv)<2:
+	print("please provide textfile name as system argument \n python bfs.py <filename>")
+	exit(0)
+print_char(sys.argv[1])
+visited=[]
+queue=[]
+
+if len(robot) == 0 or len(box) == 0 or len(storage) == 0 or len(walls) == 0:
+	print("please provide the textfile in write format Walls :O \n storage : S \n box : B \n robot : R \n box on storage : . \n robot on storage : $ \n should include walls,storage,box,robot")
+	exit(0)
+
+#function for movement of a box
+
+def move(point,dir,path,temp_box_list):
+	box_list=[]
+	box_list=temp_box_list[:]
+	temp_append=[]
+	cur_path=[]
+	cur_path=path[:]
+	cur_path.append(dir)
+	if point not in walls:
+		if point in box_list:
+			ind=box_list.index(point)
+			temp_box=[x + y for x, y in zip(point, directions[dir])]
+			if temp_box not in box_list and temp_box not in walls:
+				box_list[ind]=[x + y for x, y in zip(point, directions[dir])]
+				temp_append.append(point)
+				for i in box_list:
+					temp_append.append(i)
+				if temp_append not in visited:
+					counter=0
+					popped_robot=temp_append[0]
+					for k in visited:
+						k_temp_visited=k[:]
+						
+						k_robot=k_temp_visited.pop(0)
+						temporary=[]
+						if k_robot==popped_robot:
+							count_list=0
+							temporary=temp_append[1:]
+							for m in k_temp_visited:
+								if m in temporary:
+									count_list=count_list+1
+							if count_list==len(temporary):
+								counter=counter+1
+					if counter==0:
+						temp_append.append(cur_path)
+						dis_append=heuristic(box_list,storage,point)
+						temp_append.append(dis_append)
+						queue.append(temp_append)
+				if set(map(tuple,box_list))==set(map(tuple,storage)):
+					stop = timeit.default_timer()
+					total_time=stop-start_time
+					print("solution found")
+					print(cur_path)
+					print("total time taken: ")
+					print(total_time)
+					print("total steps take :")
+					print(len(cur_path))
+					exit()
+		else:
+			temp_append.append(point)
+			for i in box_list:
+				temp_append.append(i)
+			if temp_append not in visited:
+				counter=0
+				popped_robot=temp_append[0]
+				for k in visited:
+					k_temp_visited=k[:]
+					count_list=0
+					k_robot=k_temp_visited.pop(0)
+					temporary=[]
+					if k_robot==popped_robot:
+						temporary=temp_append[1:]
+						for m in k_temp_visited:
+							if m in temporary:
+								count_list=count_list+1
+						if count_list==len(temporary):
+							counter=counter+1
+				if counter==0:	
+					dis_append=heuristic(box_list,storage,point)
+					temp_append.append(cur_path)
+					temp_append.append(dis_append)
+					
+					queue.append(temp_append)
+			if set(map(tuple,box_list))==set(map(tuple,storage)):
+				stop = timeit.default_timer()
+				total_time=stop-start_time
+				print("solution found")
+				print(cur_path)
+				print("total time taken: ")
+				print(total_time)
+				print("total steps take :")
+				print(len(cur_path))
+				exit()
+#directions
+dir_N='N'
+dir_S='S'
+dir_E='E'
+dir_W='W'
+
+#function for greedy_heuristic
+
+def greedy_heuristic():
+	start_time = timeit.default_timer()
+	temp_queue=[]
+	initial=robot[0]
+	temp_queue.append(initial)
+	for i in box:
+		temp_queue.append(i)
+	path=[]
+	temp_queue.append(path)
+	temp_queue.append(0)
+	queue.append(temp_queue)
+	count=0
+	while queue:
+		temp_box_list=[]
+		visited_adding=[]
+		queue.sort(key=lambda x: x[-1])
+		robot_position_list=queue.pop(0)
+		robot_position=robot_position_list[0]
+		visited_adding=robot_position_list[:-2]
+		if visited_adding not in visited:
+			visited.append(visited_adding)
+		temp_path=robot_position_list[-2][:]
+		temp_box_list=robot_position_list[1:-2]
+		N=[x + y for x, y in zip(robot_position, directions['N'])]
+		S=[x + y for x, y in zip(robot_position, directions['S'])]
+		E=[x + y for x, y in zip(robot_position, directions['E'])]
+		W=[x + y for x, y in zip(robot_position, directions['W'])]
+		move(N,dir_N,temp_path,temp_box_list)
+		move(S,dir_S,temp_path,temp_box_list)
+		move(E,dir_E,temp_path,temp_box_list)
+		move(W,dir_W,temp_path,temp_box_list)
+		count=count+1
+		if not queue:
+			print("solution not found")
+			exit()
+greedy_heuristic()
+
+
